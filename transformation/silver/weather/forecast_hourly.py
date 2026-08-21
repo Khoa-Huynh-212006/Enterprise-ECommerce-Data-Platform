5b6087c707b39ca3d42dace01daf59c86187f0b4
24522074@ms.uit.edu.vn <24522074@ms.uit.edu.vn>
2026-08-21 02:06:51 +0000

    Hoàn thành hàm làm phẳng hourly và join response và metadata

--- transformation/silver/weather/forecast_hourly.py
+++ transformation/silver/weather/forecast_hourly.py
@@ -2,6 +2,7 @@ from pyspark.sql import DataFrame, SparkSession
 from delta.tables import DeltaTable
 
 
+# Load df_metadata và df_response từ Ingestion_Path
 def load_forecast_bronze(
     spark: SparkSession, 
     ingestion_path: str
@@ -13,7 +14,7 @@ def load_forecast_bronze(
     df_response = spark.read.format("json").option("multiline", True).load(f"{ingestion_path}/response.json")
     return df_response, df_metadata
     
-
+# Lấy ra danh sách các Paths đã commit (_SUCCESS)
 def discover_committed_forecast_ingestions(
     bronze_client,
     forecast_root: str,
@@ -39,6 +40,7 @@ def discover_committed_forecast_ingestions(
 
     return sorted(committed_ingestion_paths)
 
+# Lấy ra danh sách các ingestion_id đã processed
 def get_processed_forecast_ingestion_ids(
     spark: SparkSession,
     silver_path: str,
@@ -62,7 +64,7 @@ def get_processed_forecast_ingestion_ids(
     }
 
 
-
+# Lấy ra danh sách các ingestion_id chưa processed
 def find_pending_forecast_ingestions(
     committed_ingestion_paths: list[str],
     processed_ingestion_ids: set[str],
@@ -75,6 +77,99 @@ def find_pending_forecast_ingestions(
             final_ingestion_paths.append(ingestion_path)
 
     return final_ingestion_paths
+
+# Load các ingestion_id chưa processed
+def load_pending_forecast_bronze(
+    spark: SparkSession,
+    pending_ingestion_paths: list[str],
+) -> tuple[DataFrame, DataFrame]:
+    if not pending_ingestion_paths:
+        raise ValueError("Không có pending Forecast ingestion để load.")
+
+    response_paths = [f"{path}/response.json" for path in pending_ingestion_paths]
+    metadata_paths = [f"{path}/metadata.json" for path in pending_ingestion_paths]
+
+    df_response = spark.read.format("json").option("multiline", True).load(response_paths) #bulk load
+    df_metadata = spark.read.format("json").option("multiline", True).load(metadata_paths)
+
+    return df_response, df_metadata
+
+
+# Thêm vào ingestion_id từ Path cho response df
+def extract_forecast_ingestion_context(
+    df_response: DataFrame,
+) -> DataFrame:
+    df_response = (
+        df_response
+        .withColumn("_source_file_path", F.col("_metadata.file_path"))
+        .withColumn("ingestion_id", F.regexp_extract("_source_file_path", r"ingestion_id=([^/]+)", 1))
+    )
+    
+    return df_response
+
+
+#Làm phẳng hourly array
+def flatten_hourly_arrays(
+    df_response: DataFrame,
+) -> DataFrame:
+
+    df_response = df_response.withColumn(
+        "hourly",
+        F.arrays_zip(
+            "hourly.time",
+            "hourly.temperature_2m",
+            "hourly.relative_humidity_2m",
+            "hourly.precipitation",
+            "hourly.wind_speed_10m",
+            "hourly.weather_code",
+        )
+    )
+
+    df_response = df_response.withColumn(
+        "hourly",
+        F.explode("hourly")
+    )
+
+    df_response = df_response.select(
+        "ingestion_id",
+        "_source_file_path",
+        "latitude",
+        "longitude",
+        "timezone",
+        F.col("hourly.time").alias("time"),
+        F.col("hourly.temperature_2m").alias("temperature_2m"),
+        F.col("hourly.relative_humidity_2m").alias("relative_humidity_2m"),
+        F.col("hourly.precipitation").alias("precipitation"),
+        F.col("hourly.wind_speed_10m").alias("wind_speed_10m"),
+        F.col("hourly.weather_code").alias("weather_code"),
+    )
+
+    return df_response
+
+
+# Join response và metadata    
+def attach_forecast_metadata(
+    df_response: DataFrame,
+    df_metadata: DataFrame,
+) -> DataFrame:
+
+    df = df_response.join(
+        df_metadata,
+        on="ingestion_id",
+        how="left",
+    )
+
+    return df
+
+
+
+
+
+
+
+
+
+
 
 
 
